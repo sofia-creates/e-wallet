ff944cf3ca1328fb829529ac8d2fef0f0eacce19
sofia-creates <sofia.ledin2@gmail.com>
2024-10-11 20:46:19 +0200

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Routes,
+  Route,
+} from "react-router-dom";
+//importera Home här efter den skapats
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
